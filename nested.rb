32f3954be8c97ec6dcabf1c10c3198ead55361a7
Travis Prol <travisjohnprol@gmail.com>
2020-05-13 13:04:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,9 +79,13 @@ def adding_matz
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
+      },
+      :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
       }
     }
-
+  programmer_hash
 end
 
 def changing_alan
